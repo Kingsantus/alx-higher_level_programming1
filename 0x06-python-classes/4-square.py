a35b2d25edceafea70f256a256a9ae52208423c4
kingsantus <nkingsantus@gmail.com>
2023-05-31 05:51:36 -0700

    Access and update private attribute

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -4,23 +4,26 @@ class Square():
     """Square class"""
     def __init__(self, size=0):
         """initialization of attr"""
-        self.size = size
+        self.__size = size
 
+    """setter method initialized"""
     @property
     def size(self):
         """get metthod for the size attr"""
         return self.__size
 
+    """getter method initializes"""
     @size.setter
     def size(self, value):
         """set method for size attr"""
-        if type(size) != int:
+        if type(value) != int:
             raise TypeError("size must be an integer")
-        elif size < 0:
+        elif value < 0:
             raise ValueError("size must be >= 0")
         """Assign to private attr"""
-        self.__size = size
+        self.__size = value
 
+    """public method initiazied"""
     def area(self):
         """public instance to return square"""
         return self.__size ** 2
