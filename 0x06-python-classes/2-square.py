4db4f88a37a5f0b5c5eced816ce1165db7124400
kingsantus <nkingsantus@gmail.com>
2023-05-31 05:07:05 -0700

    Size Validation

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -1,11 +1,12 @@
 #!/usr/bin/python3
+"""square module"""
 class Square():
     """square class"""
     def __init__(self, size=0):
         """initialization of instance"""
-        if not isinstance(size, int):
+        if type(size) != int:
             raise TypeError("size must be an integer")
-        if size < 0:
+        elif size < 0:
             raise ValueError("size must be >= 0")
         """Assign to private attr"""
         self.__size = size
