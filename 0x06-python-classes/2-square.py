42d0fc12bb7fcd8b4a07e470329db8547328512a
kingsantus <nkingsantus@gmail.com>
2023-05-31 05:03:02 -0700

    Size Vallidation

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -3,11 +3,9 @@ class Square():
     """square class"""
     def __init__(self, size=0):
         """initialization of instance"""
-        if type(size) != int:
-            """check if size is an integer"""
+        if not isinstance(size, int):
             raise TypeError("size must be an integer")
-        elif size < 0:
-            """check if size is non-negative"""
+        if size < 0:
             raise ValueError("size must be >= 0")
         """Assign to private attr"""
         self.__size = size
