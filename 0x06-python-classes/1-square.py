c25471ae5a725a85c342c6d08a3a73df4d4b99c2
kingsantus <nkingsantus@gmail.com>
2023-05-31 04:43:19 -0700

    Square with size

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+"""square module code """
+class Square():
+    """ square class """
+    def __init__(self, size):
+        """ initialize instance of the class """
+        self.__size = size
