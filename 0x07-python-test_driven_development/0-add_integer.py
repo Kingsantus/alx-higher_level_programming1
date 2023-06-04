aeb68efbc6a583bebbf2e3fc72ad7f61af823ce1
kingsantus <nkingsantus@gmail.com>
2023-06-04 16:35:57 -0700

    Integers addition

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -1,5 +1,16 @@
 #!/usr/bin/python3
+""" add_integer module """
 def add_integer(a, b=98):
+    """
+    return the sum a and b
+
+    Args:
+        a: (int) 
+        b=98: (int) with 98 default
+
+    Raise:
+        TypeError: if a is not int or b is not int
+    """
     if type(a) not in (int, float):
         raise TypeError("a must be an integer")
     if type(b) not in (int, float):
