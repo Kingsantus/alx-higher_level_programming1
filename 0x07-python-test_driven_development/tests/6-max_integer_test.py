92ece7d7ab375564fbf9945d2ed31c850c7e58fb
kingsantus <nkingsantus@gmail.com>
2023-06-04 05:53:19 -0700

    Test-driven development

---
+++ 0x07-python-test_driven_development/tests/6-max_integer_test.py
@@ -0,0 +1,36 @@
+import unittest
+from max_integer import max_integer
+
+class TestMaxInteger(unittest.TestCase):
+    def test_empty_list(self):
+        """Test when the list is empty"""
+        result = max_integer([])
+        self.assertIsNone(result)
+
+    def test_single_element_list(self):
+        """Test when the list has a single element"""
+        result = max_integer([5])
+        self.assertEqual(result, 5)
+
+    def test_positive_integers(self):
+        """Test when the list has positive integers"""
+        result = max_integer([1, 2, 3, 4, 5])
+        self.assertEqual(result, 5)
+
+    def test_negative_integers(self):
+        """Test when the list has negative integers"""
+        result = max_integer([-1, -2, -3, -4, -5])
+        self.assertEqual(result, -1)
+
+    def test_mixed_integers(self):
+        """Test when the list has both positive and negative integers"""
+        result = max_integer([-5, 2, -10, 6, 0])
+        self.assertEqual(result, 6)
+
+    def test_duplicate_maximum(self):
+        """Test when the list has duplicate maximum values"""
+        result = max_integer([1, 2, 3, 4, 5, 5, 5])
+        self.assertEqual(result, 5)
+
+if __name__ == '__main__':
+    unittest.main()
