c4e11ed74c26746868e9449764a9007fdcf37d61
kingsantus <nkingsantus@gmail.com>
2023-06-05 00:48:40 -0700

    Print square

--- 0x07-python-test_driven_development/4-print_square.py
+++ 0x07-python-test_driven_development/4-print_square.py
@@ -1,9 +1,21 @@
 #!/usr/bin/python3
+"""
+print_square module
+this module receives args
+multipy # by args
+if args is not an int or float raise TypeError
+if args is less than 0 raiseVvalueError
+"""
 def print_square(size):
+    """
+    print # * size
+    """
     if not isinstance(size, int):
         raise TypeError("size must be an integer")
     if size < 0:
         raise ValueError("size must be >= 0")
+    if isinstance(size, float) and size < 0:
+        raise TypeError("size must be an integer")
 
     for _ in range(size):
         print("#" * size)
