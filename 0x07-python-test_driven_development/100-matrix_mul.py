c8bba4319839c50f0f55edc16ae428febc2ffc21
kingsantus <nkingsantus@gmail.com>
2023-06-05 05:21:30 -0700

    Matrix multiplication

--- 0x07-python-test_driven_development/100-matrix_mul.py
+++ 0x07-python-test_driven_development/100-matrix_mul.py
@@ -1,39 +1,43 @@
 #!/usr/bin/python3
+"""matrix_mul module multiply element of two list 
+return a new list"""
+
 def matrix_mul(m_a, m_b):
-    """Validate if m_a is a list"""
+    """matrix_mul multiply argument list provided"""
     if not isinstance(m_a, list):
+        """Validate if m_a is a list"""
         raise TypeError("m_a must be a list")
 
-    """Validate if m_b is a list"""
     if not isinstance(m_b, list):
+        """Validate if m_b is a list"""
         raise TypeError("m_b must be a list")
 
-    """Validate if m_a is a list of lists"""
     if any(not isinstance(row, list) for row in m_a):
+        """Validate if m_a is a list of lists"""
         raise TypeError("m_a must be a list of lists")
 
-    """Validate if m_b is a list of lists"""
     if any(not isinstance(row, list) for row in m_b):
+        """Validate if m_b is a list of lists"""
         raise TypeError("m_b must be a list of lists")
 
-    """Validate if m_a is not empty"""
     if len(m_a) == 0 or any(len(row) == 0 for row in m_a):
+        """Validate if m_a is not empty"""
         raise ValueError("m_a can't be empty")
 
-    """Validate if m_b is not empty"""
     if len(m_b) == 0 or any(len(row) == 0 for row in m_b):
+        """Validate if m_b is not empty"""
         raise ValueError("m_b can't be empty")
 
-    """Validate if m_a contains only integers or floats"""
     if any(not isinstance(element, (int, float)) for row in m_a for element in row):
+        """Validate if m_a contains only integers or floats"""
         raise TypeError("m_a should contain only integers or floats")
 
-    """Validate if m_b contains only integers or floats"""
     if any(not isinstance(element, (int, float)) for row in m_b for element in row):
+        """Validate if m_b contains only integers or floats"""
         raise TypeError("m_b should contain only integers or floats")
 
-    """Validate if m_a and m_b are rectangles"""
     a_num_columns = len(m_a[0])
+    """Validate if m_a and m_b are rectangles"""
     if any(len(row) != a_num_columns for row in m_a):
         raise ValueError("each row of m_a must be of the same size")
 
@@ -41,12 +45,12 @@ def matrix_mul(m_a, m_b):
     if any(len(row) != b_num_columns for row in m_b):
         raise ValueError("each row of m_b must be of the same size")
 
-    """Validate if m_a and m_b can be multiplied"""
-    if a_num_columns != len(m_b):
+    if a_num_columns != b_num_columns:
+        """Validate if m_a and m_b can be multiplied"""
         raise ValueError("m_a and m_b can't be multiplied")
 
-    """Perform matrix multiplication"""
     result = []
+    """Perform matrix multiplication"""
     for i in range(len(m_a)):
         row = []
         for j in range(len(m_b[0])):
