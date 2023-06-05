47bf9afc6e579c64b0538d01efd2f79e36b01d2a
kingsantus <nkingsantus@gmail.com>
2023-06-05 06:22:31 -0700

    Lazy matrix multiplication

--- 0x07-python-test_driven_development/101-lazy_matrix_mul.py
+++ 0x07-python-test_driven_development/101-lazy_matrix_mul.py
@@ -1,7 +1,14 @@
 #!/usr/bin/python3
+"""lazy_matrix_mul module multiply list using numpy()
+"""
 import numpy as np
 
 def lazy_matrix_mul(m_a, m_b):
+    """lazy_matrix_mull() takes tor args
+    convert to np array
+    using np multiplication method to multipy array
+    raise error if a string
+    """
     try:
         np_a = np.array(m_a)
         np_b = np.array(m_b)
