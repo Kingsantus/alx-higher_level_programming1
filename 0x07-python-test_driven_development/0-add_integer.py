ae530f28f62a7d14c2d61c3f2eb2857d6eeef7ad
kingsantus <nkingsantus@gmail.com>
2023-06-05 00:07:03 -0700

    Integers addition

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -1,16 +1,13 @@
 #!/usr/bin/python3
-""" add_integer module """
+""" 
+add_integer module receives two args
+this module adds two args provide
+if args 1 or args 2 is not an int or float Raise TypeError
+"""
 def add_integer(a, b=98):
-    """
-    return the sum a and b
-
-    Args:
-        a: (int) 
-        b=98: (int) with 98 default
-
+    """ return the sum a and b
     Raise:
-        TypeError: if a is not int or b is not int
-    """
+        TypeError: if a is not int or b is not int """
     if type(a) not in (int, float):
         raise TypeError("a must be an integer")
     if type(b) not in (int, float):
