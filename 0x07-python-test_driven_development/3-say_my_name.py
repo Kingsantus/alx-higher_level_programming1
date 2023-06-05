68ab690537daf014834b44db3830d5aeab28eefb
kingsantus <nkingsantus@gmail.com>
2023-06-05 00:27:21 -0700

    Say my name

--- 0x07-python-test_driven_development/3-say_my_name.py
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -1,5 +1,14 @@
 #!/usr/bin/python3
+"""
+say_my_name module concantenate two Args with a space
+if agrs is not string raise TypeError
+if only one args provide print else case
+if both argument is provide print if case
+"""
 def say_my_name(first_name, last_name=""):
+    """say_my_name() receives to arg
+    concatenate and print
+    if one arg print too"""
     if not isinstance(first_name, str):
         raise TypeError("first_name must be a string")
     if not isinstance(last_name, str):
