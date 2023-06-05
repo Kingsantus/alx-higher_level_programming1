2b314bb86da8a29944dd1cb20b1da2db04b494ba
kingsantus <nkingsantus@gmail.com>
2023-06-05 01:10:33 -0700

    Text indentation

--- 0x07-python-test_driven_development/5-text_indentation.py
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -1,5 +1,16 @@
 #!/usr/bin/python3
+"""
+text_indentation module
+takes an args which is a strings of text
+loop through the args if it encounter [.,:,?] and '\'
+it create a newline for the args
+"""
 def text_indentation(text):
+    """
+    text_indentation() check if the arg provide is string if not raise TypeError
+    it loop through and ensure that if the condition of [.,:,?] and '\'
+    makes a new line until nothing remain
+    """
     if not isinstance(text, str):
         raise TypeError("text must be a string")
     char_remove = [".", ":", "?"]
