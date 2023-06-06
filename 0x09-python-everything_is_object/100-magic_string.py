807045f3e20186c10946f2a4d0f5e3ff38ad8916
kingsantus <nkingsantus@gmail.com>
2023-06-06 15:18:31 -0700

    #pythonic

--- 0x09-python-everything_is_object/100-magic_string.py
+++ 0x09-python-everything_is_object/100-magic_string.py
@@ -1,7 +1,4 @@
 #!/usr/bin/python3
 def magic_string():
-    if not hasattr(magic_string, "counter"):
-        magic_string.counter = 1
-    else:
-        magic_string.counter += 1
-    return "BestShool" * magic_string.counter
+    magic_string.counter = getattr(magic_string, 'counter', 0) + 1
+    return ", ".join(["BestSchool"] * magic_string.counter)
