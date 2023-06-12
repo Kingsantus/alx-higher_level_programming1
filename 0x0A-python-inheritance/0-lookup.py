98114f3091e96f3b09f35b0590603d904d82f35a
kingsantus <nkingsantus@gmail.com>
2023-06-12 14:38:13 -0700

    Lookup

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -1,3 +1,13 @@
 #!/usr/bin/python3
+"""0-lookup module"""
 def lookup(obj):
+    """
+    looks up the available attributes and methods of an object
+
+    Args:
+        obj: object to look up
+
+    Return:
+        the list of available dir in the obj
+    """
     return dir(obj)
