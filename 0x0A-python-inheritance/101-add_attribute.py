a5446b8c0e75fc3951fff4a4cf0fd695be74d2ba
kingsantus <nkingsantus@gmail.com>
2023-06-12 15:22:03 -0700

    Can I?

--- 0x0A-python-inheritance/101-add_attribute.py
+++ 0x0A-python-inheritance/101-add_attribute.py
@@ -1,6 +1,18 @@
 #!/usr/bin/python3
-def add_attribute(obj, attr_name, attr_value):
-    if not hasattr(obj, '__dict__'):
-        raise TypeError("can't add new attribute")
+"""101-add_attribute module"""
+def add_attribute(obj, attr, value):
+        """
+    adds attribute to an object if it is possible
+
+    Args:
+        obj: object to receive new attributes
+        attr: attribute name
+        value: attribute value
+
+    Raise(s):
+        TypeError: if new attribute can't be added
+    """
+    if hasattr(obj, '__dict__'):
+        setattr(obj, attr, value)
     else:
-        obj.__dict__[attr_name] = attr_value
+        raise TypeError("can't add new attribute")
