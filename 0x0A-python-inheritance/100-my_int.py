a4d5998478ab178cc2b150dd9752bdb85c94c2ea
kingsantus <nkingsantus@gmail.com>
2023-06-12 14:58:38 -0700

    My integer

--- 0x0A-python-inheritance/100-my_int.py
+++ 0x0A-python-inheritance/100-my_int.py
@@ -1,4 +1,10 @@
+#!/usr/bin/python3
+"""100-my_int"""
 class MyInt(int):
+    """Myint class"""
+    def __init__(self, value):
+        """Instantiates object"""
+        self.value = value
     def __eq__(self, other):
         return super().__ne__(other)
 
