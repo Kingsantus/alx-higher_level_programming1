09ccdd0b986041bad102da212dceb94c7397225f
kingsantus <nkingsantus@gmail.com>
2023-06-12 21:49:05 -0700

    . Improve Geometry

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+"""6-base_geometry module """
+class BaseGeometry:
+    """ BaseGeometry class area func not defined, throw error when area is intracted with """
+    def area(self):
+        raise Exception("area() is not implemented")
