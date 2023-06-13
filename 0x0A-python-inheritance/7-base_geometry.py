119b4f5a67aa7631c25fc0cc85e359db6e661327
kingsantus <nkingsantus@gmail.com>
2023-06-12 22:18:52 -0700

    Integer validator

--- 0x0A-python-inheritance/7-base_geometry.py
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""6-base_geometry module """
+"""7-base_geometry module """
 class BaseGeometry:
     """ BaseGeometry class area func not defined, throw error when area is intracted with """
     def area(self):
