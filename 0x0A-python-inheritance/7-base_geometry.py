2ebcc3d18041c2ca61c7145675b5aaca9ca47899
kingsantus <nkingsantus@gmail.com>
2023-06-12 22:10:44 -0700

    Integer validator

--- 0x0A-python-inheritance/7-base_geometry.py
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -1,13 +1,12 @@
 #!/usr/bin/python3
-""" 7-base_geometry module """
+"""6-base_geometry module """
 class BaseGeometry:
-     """ BaseGeometry class area func not defined, throw error when area is intracted with """
-
+    """ BaseGeometry class area func not defined, throw error when area is intracted with """
     def area(self):
         raise Exception("area() is not implemented")
 
     def integer_validator(self, name, value):
-        """ integer_validator function check if value is int"""
+        """integer_validator ensure value is a positive integer"""
         if not isinstance(value, int):
             raise TypeError("{} must be an integer".format(name))
         if value <= 0:
