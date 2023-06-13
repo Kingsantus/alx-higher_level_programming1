75f9c0830cdd0455a419481c3b97871437796f20
kingsantus <nkingsantus@gmail.com>
2023-06-12 21:16:29 -0700

    Same class or inherit from

---
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+"""3-is_kind_of_class module"""
+def is_kind_of_class(obj, a_class):
+    """ is_kind_of_class function checks if obj is part of a_class"""
+    return isinstance(obj, a_class)
