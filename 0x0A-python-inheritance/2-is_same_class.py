530413488d1214b52f93bf00bf71ad92e6fb01b5
kingsantus <nkingsantus@gmail.com>
2023-06-12 21:10:35 -0700

    Exact same object

---
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+"""2-is_same_class module"""
+
+def is_same_class(obj, a_class):
+    """is_same_class function check through code
+    Returns: true or false"""
+    return type(obj) is a_class
