bd65807f62e1a65cc9cf47917fa22acae1177f62
kingsantus <nkingsantus@gmail.com>
2023-06-13 21:59:42 -0700

    Pascal's Triangle

---
+++ 0x0B-python-input_output/12-pascal_triangle.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""12-pascal_triangle module"""
+def pascal_triangle(n):
+    """pascal_triangle() return a row of pyramid list"""
+    if n <= 0:
+        return []
+
+    triangle = [[1]]
+
+    for i in range(1, n):
+        row = [1]
+        prev_row = triangle[i - 1]
+
+        for j in range(1, i):
+            row.append(prev_row[j - 1] + prev_row[j])
+
+        row.append(1)
+        triangle.append(row)
+
+    return triangle
